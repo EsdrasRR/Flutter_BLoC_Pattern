9c4b935eb8d4c96095d3275b3f0f34ad5ea3a3ec
Esdras Rafael <esdrasrocha1122@gmail.com>
2019-11-07 22:53:31 -0300

    Using BLoC pattern

--- bloc_pattern/lib/hero_model.dart
+++
@@ -1,7 +0,0 @@
-class hero_model{
-
-  String name;
-  bool isFavorite;
-
-  hero_model({this.name, this.isFavorite});
-}
--- bloc_pattern/lib/main.dart
+++ bloc_pattern/lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'home.dart';
+import 'myhomepage.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: HomeWidget(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(title: 'BLoC'),
     );
   }
 }
